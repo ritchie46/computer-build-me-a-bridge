c2de954584205a826e9c48038da8825d8eba2510
Ritchie <ritchie46@gmail.com>
2018-01-27 18:13:06 +0100

    Made the hidden layers visible to pytorch

--- evolution_strategies/model.py
+++ evolution_strategies/model.py
@@ -14,11 +14,15 @@ class Model(torch.nn.Module):
         :param hidden_neurons: (tpl) Hidden neuron per hidden layer.
         """
         super(Model, self).__init__()
+        self.n_in = n_in
+        self.n_out = n_out
+        self.hidden_layers = hidden_layers
+        self.hidden_neurons = hidden_neurons
         self.dense_in = nn.Linear(n_in, hidden_neurons[0])
         self.hidden = []
-
         for i in range(hidden_layers):
-            self.hidden.append(nn.Linear(hidden_neurons[i], hidden_neurons[i]))
+            exec(f"self.hidden_{i + 1} = nn.Linear(hidden_neurons[{i}], hidden_neurons[{i}])")
+            exec(f"self.hidden.append(self.hidden_{i + 1})")
         self.dense_out = nn.Linear(hidden_neurons[-1], n_out)
 
     def forward(self, x):
@@ -29,10 +33,19 @@ class Model(torch.nn.Module):
 
         return self.dense_out(x)
 
+    def es_params(self):
+        """
+        The parameters that will be trained by ES.
+        :return: (list)
+        """
+        return [(k, v) for k, v in zip(self.state_dict().keys(),
+                                       self.state_dict().values())]
+
 
 if __name__ == "__main__":
     # test a model
     from torch.autograd import Variable
     m = Model(2, 10, 1)
+    print(m)
     x = Variable(torch.FloatTensor([1, 1]))
     print(m(x))
