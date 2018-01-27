4ecd9af6c69ca2441f98802adab7681a25aed997
Ritchie <ritchie46@gmail.com>
2018-01-27 18:28:24 +0100

    Pertubate function

---
+++ evolution_strategies/train.py
@@ -0,0 +1,37 @@
+from evolution_strategies.model import Model
+import numpy as np
+import torch
+
+
+def perturbate(model, seed, sigma=0.05):
+    """
+    Create two new models. And modify both in opposite direction.
+
+    :param model: (Model)
+    :param seed: (int)
+    :param sigma: (flt)
+    :return: (Model, Model)
+    """
+    args = [model.n_in, model.n_out, model.hidden_layers, model.hidden_neurons]
+
+    new_model = Model(*args)
+    anti_model = Model(*args)
+    new_model.load_state_dict(model.state_dict())
+    anti_model.load_state_dict(model.state_dict())
+    np.random.seed(seed)
+
+    for (k, v), (anti_k, anti_v) in zip(new_model.es_params(),
+                                        anti_model.es_params()):
+
+        # draw from a gaussian distribution eps ~phi(mean, std)
+        eps = np.random.normal(0, 1, v.size())
+        noise = torch.from_numpy(sigma * eps).float()
+        v += noise
+        anti_v -= noise
+
+    return new_model, anti_model
+
+m = Model(2, 10, 1)
+
+perturbate(m, 1, 0.05)
+
